33161047a7efb3d87f7753fce515545c7d80147e
Devin Spikowski <vegeta897@gmail.com>
2026-06-30 18:10:14 -0400

    Skip download if file exists

--- download.ts
+++ download.ts
@@ -13,25 +13,30 @@ export async function downloadItem(item: FeedItem) {
   }
   let contentLength = +(response.headers.get('Content-Length') ?? 0)
   if (contentLength === 0) contentLength = +item.enclosure.length
-  let receivedLength = 0
-  let percent = 0
-  const chunks = []
-  console.log(`Downloading "${item.title}"`)
-  for await (const chunk of response.body) {
-    chunks.push(chunk)
-    receivedLength += chunk.length
-    const nextPercent = Math.floor((receivedLength / contentLength) * 100)
-    if (nextPercent > percent) {
-      percent = nextPercent
-      process.stdout.write('\r\x1b[K') // Return to beginning of line and clear to end
-      process.stdout.write(`${percent}%`)
-    }
-  }
-  process.stdout.write('\r\x1b[K')
-  const blob = new Blob(chunks)
   const path = getFilepath(item)
-  console.log(`Saving video to ${path}`)
-  await Bun.write(path, blob)
+  const existingFile = Bun.file(path)
+  if (existingFile.size === contentLength) {
+    console.log(`File with matching name and size already exists at ${path}`)
+  } else {
+    let receivedLength = 0
+    let percent = 0
+    const chunks = []
+    console.log(`Downloading "${item.title}" (${item.guid})`)
+    for await (const chunk of response.body) {
+      chunks.push(chunk)
+      receivedLength += chunk.length
+      const nextPercent = Math.floor((receivedLength / contentLength) * 100)
+      if (nextPercent > percent) {
+        percent = nextPercent
+        process.stdout.write('\r\x1b[K') // Return to beginning of line and clear to end
+        process.stdout.write(`${percent}%`)
+      }
+    }
+    process.stdout.write('\r\x1b[K')
+    const blob = new Blob(chunks)
+    console.log(`Saving video to ${path}`)
+    await Bun.write(path, blob)
+  }
   recordItemDownloaded(item.guid)
   return true
 }
